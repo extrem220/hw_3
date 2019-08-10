30515bb53601030a93b7f2fceb22c65f799cdd62
andrewov <nothing.type@gmail.com>
2019-08-10 14:57:30 +0300

    add sort

--- src/hw3/llist.js
+++ src/hw3/llist.js
@@ -27,7 +27,6 @@ const LList = function () {
 
         return root;
     };
-
     this.getSizeList = () => {
         if (!root) {
             return size;
@@ -46,7 +45,6 @@ const LList = function () {
 
         return size;
     };
-
     this.pushList = value => {
 
         if (!root) {
@@ -64,7 +62,6 @@ const LList = function () {
 
         return root;
     };
-
     this.popList = () => {
         if (!root) {
             return null;
@@ -87,4 +84,145 @@ const LList = function () {
         return value;
 
     }
+    this.unshiftList = value => {
+        if (!root) {
+            root = new Node(value);
+            return root;
+        }
+
+        let current = root;
+
+        let temp = new Node(value);
+        temp.next = current;
+        root = temp;
+
+        return root;
+
+    };
+    this.shiftList = () => {
+        if (!root) {
+            return null;
+        }
+
+        let value = root.value;
+
+        if (root.next === null) {
+            root = null;
+        } else {
+            root = root.next;
+        }
+
+        return value;
+    };
+    this.getList = index => {
+        if (!root || index < 0) {
+            return null;
+        }
+
+        let count = 0;
+        let value = root.value;
+
+        while (count !== index) {
+            if (root.next === null) {
+                return null;
+            }
+
+            root = root.next;
+            count++;
+        }
+
+        value = root.value;
+
+        return value;
+    };
+    this.setList = (index, value) => {
+        if (!root || index < 0) {
+            if (index === 0) {
+                root = new Node(value);
+            } else {
+                return null;
+            }
+        }
+
+        let count = 0;
+        let current = root;
+
+        if (index !== null) {
+            while (count !== index) {
+                if (current.next === null) {
+                    return null;
+                }
+
+                current = current.next;
+                count++;
+            }
+
+            current.value = value;
+        }
+
+        return root;
+    };
+    this.sortList = (first, second) => {
+        if (!root || this.getSizeList() === 0) {
+            return null;
+        }
+
+        if (this.getSizeList() === 1) {
+            return root;
+        }
+
+        if (this.compare(first, second)) {
+            let current = root;
+            let temp = root;
+
+            for(let i = this.getSizeList(); i > 0; i--){
+                for(let j = 0; j < i; j++) {
+                    if (current.next !== null) {
+                        if (current.value > current.next.value) {
+                            temp = current.value;
+                            current.value = current.next.value;
+                            current.next.value = temp;
+                        }
+
+                        current = current.next;
+                    }
+                }
+
+                current = root;
+                temp = root;
+            }
+
+            return root;
+        } else {
+            let current = root;
+            let temp = root;
+
+            for(let i = this.getSizeList(); i > 0; i--){
+                for(let j = 0; j < i; j++) {
+                    if (current.next !== null) {
+                        if (current.value < current.next.value) {
+                            temp = current.value;
+                            current.value = current.next.value;
+                            current.next.value = temp;
+                        }
+
+                        current = current.next;
+                    }
+                }
+
+                current = root;
+                temp = root;
+            }
+
+            return root;
+        }
+    };
+
+    this.compare = (first, second) => {
+        if (!first && !second) {
+            return false;
+        }
+
+        return first <= second ? true : false;
+    };
 };
--- tests/hw3/llist.test.js
+++ tests/hw3/llist.test.js
@@ -216,4 +216,413 @@ describe('HW3 LList', () => {
             })
         })
     });
+    describe('unshiftList', () => {
+        // GIVEN
+        const testData = [
+            {
+                array: [1, 2, 3, 4, 5],
+                value: -5,
+                expected: {
+                    value: -5,
+                    next: {
+                        value: 1,
+                        next: {
+                            value: 2,
+                            next: {
+                                value: 3,
+                                next: {
+                                    value: 4,
+                                    next: {
+                                        value: 5,
+                                        next: null
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+            },
+            {
+                array: [],
+                value: 1,
+                expected: {value: 1, next: null},
+            },
+            {
+                array: null,
+                value: 0,
+                expected: {value: 0, next: null},
+            },
+            {
+                array: [1],
+                value: 2,
+                expected: {
+                    value: 2,
+                    next: {
+                        value: 1,
+                        next: null
+                    }
+                },
+            },
+            {
+                array: [1, 2],
+                value: 9,
+                expected: {
+                    value: 9,
+                    next: {
+                        value: 1,
+                        next: {
+                            value: 2,
+                            next: null
+                        }
+                    }
+                },
+            },
+        ];
+
+        //WHEN
+        testData.forEach(data => {
+            const {array, value, expected} = data;
+
+            const unshiftL = new LList();
+            unshiftL.initList(array);
+            const actual = unshiftL.unshiftList(value);
+
+            //THEN
+            it(`should return ${expected} when array = [${[array]}] and value = ${value}`, () => {
+                assert.deepEqual(actual, expected);
+            })
+        })
+    });
+    describe('shiftList', () => {
+        // GIVEN
+        const testData = [
+            {
+                array: [1, 2, 3, 4, 5],
+                expected: 1,
+            },
+            {
+                array: [],
+                expected: null,
+            },
+            {
+                array: null,
+                expected: null,
+            },
+            {
+                array: [1, 2],
+                expected: 1,
+            },
+            {
+                array: [1],
+                expected: 1,
+            },
+        ];
+
+        //WHEN
+        testData.forEach(data => {
+            const {array, expected} = data;
+
+            const shiftL = new LList();
+            shiftL.initList(array);
+            const actual = shiftL.shiftList();
+
+            //THEN
+            it(`should return ${expected} when array = [${[array]}]`, () => {
+                assert.deepEqual(actual, expected);
+            })
+        })
+    });
+    describe('getList', () => {
+        // GIVEN
+        const testData = [
+            {
+                array: [1, 2, 3, 4, 5],
+                value: 0,
+                expected: 1,
+            },
+            {
+                array: [1, 2, 3, 4, 5],
+                value: -2,
+                expected: null,
+            },
+            {
+                array: [],
+                value: -7,
+                expected: null,
+            },
+            {
+                array: [],
+                value: 7,
+                expected: null,
+            },
+            {
+                array: null,
+                value: null,
+                expected: null,
+            },
+            {
+                array: [1, 2],
+                value: 1,
+                expected: 2,
+            },
+            {
+                array: [1],
+                value: 0,
+                expected: 1,
+            },
+        ];
+
+        //WHEN
+        testData.forEach(data => {
+            const {array, value, expected} = data;
+
+            const getL = new LList();
+            getL.initList(array);
+            const actual = getL.getList(value);
+
+            //THEN
+            it(`should return ${expected} when array = [${[array]}] and value = ${value}`, () => {
+                assert.deepEqual(actual, expected);
+            })
+        })
+    });
+    describe('setList', () => {
+        // GIVEN
+        const testData = [
+            {
+                array: [1, 2, 3, 4, 5],
+                index: 3,
+                value: 243,
+                expected: {
+                    value: 1,
+                    next: {
+                        value: 2,
+                        next: {
+                            value: 3,
+                            next: {
+                                value: 243,
+                                next: {
+                                    value: 5,
+                                    next: null
+                                }
+                            }
+                        }
+                    }
+                },
+            },
+            {
+                array: [],
+                index: 0,
+                value: 0,
+                expected: {value: 0, next: null},
+            },
+            {
+                array: null,
+                index: null,
+                value: null,
+                expected: null,
+            },
+            {
+                array: [1],
+                index: 0,
+                value: 6,
+                expected: {
+                    value: 6,
+                    next: null
+                },
+            },
+            {
+                array: [1, 2],
+                index: 8,
+                value: 4,
+                expected: null,
+            },
+            {
+                array: [1, 2],
+                index: -3,
+                value: 4,
+                expected: null,
+            },
+            {
+                array: [1, 2],
+                index: null,
+                value: 4,
+                expected: {
+                    value: 1,
+                    next: {
+                        value: 2,
+                        next: null
+                    }
+                },
+            },
+        ];
+
+        //WHEN
+        testData.forEach(data => {
+            const {array, index, value, expected} = data;
+
+            const setL = new LList();
+            setL.initList(array);
+            const actual = setL.setList(index, value);
+
+            //THEN
+            it(`should return ${expected} when array = [${[array]}] , index = ${index}, and value = ${value}`, () => {
+                assert.deepEqual(actual, expected);
+            })
+        })
+    });
+    describe('sortList', () => {
+        // GIVEN
+        const testData = [
+            {
+                array: [1, 2, 3, 4, 5],
+                first: 3,
+                second: 243,
+                expected: {
+                    value: 1,
+                    next: {
+                        value: 2,
+                        next: {
+                            value: 3,
+                            next: {
+                                value: 4,
+                                next: {
+                                    value: 5,
+                                    next: null,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            {
+                array: [],
+                first: 1,
+                second: 2,
+                expected: null,
+            },
+            {
+                array: null,
+                first: 4,
+                second: 5,
+                expected: null,
+            },
+            {
+                array: [1],
+                first: 0,
+                second: 0,
+                expected: {
+                    value: 1,
+                    next: null,
+                },
+            },
+            {
+                array: [56, 2],
+                first: 2,
+                second: 5,
+                expected: {
+                    value: 56,
+                    next: {
+                        value: 2,
+                        next: null,
+                    },
+                },
+            },
+            {
+                array: [100, 2, 5, 1, 58, 136],
+                first: -3,
+                second: 4,
+                expected: {
+                    value: 1,
+                    next: {
+                        value: 2,
+                        next: {
+                            value: 5,
+                            next: {
+                                value: 58,
+                                next: {
+                                    value: 100,
+                                    next: {
+                                        value: 136,
+                                        next: null,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            {
+                array: [1, 2],
+                first: null,
+                second: 4,
+                expected: {
+                    value: 1,
+                    next: {
+                        value: 2,
+                        next: null,
+                    }
+                },
+            },
+            {
+                array: [100, 2, 5, 1, 58, 136],
+                first: 16,
+                second: 4,
+                expected: {
+                    value: 136,
+                    next: {
+                        value: 100,
+                        next: {
+                            value: 58,
+                            next: {
+                                value: 5,
+                                next: {
+                                    value: 2,
+                                    next: {
+                                        value: 1,
+                                        next: null,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            {
+                array: [1, 2],
+                first: 4,
+                second: null,
+                expected: {
+                    value: 2,
+                    next: {
+                        value: 1,
+                        next: null,
+                    },
+                },
+            },
+            {
+                array: [57],
+                first: 7,
+                second: 0,
+                expected: {
+                    value: 57,
+                    next: null,
+                },
+            },
+        ];
+
+        //WHEN
+        testData.forEach(data => {
+            const {array, first, second, expected} = data;
+
+            const sortL = new LList();
+            sortL.initList(array);
+            const actual = sortL.sortList(first, second);
+
+            //THEN
+            it(`should return ${expected} when array = [${[array]}] , first = ${index}, and second = ${value}`, () => {
+                assert.deepEqual(actual, expected);
+            })
+        })
+    });
 });
